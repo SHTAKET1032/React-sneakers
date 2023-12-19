a877adf56e7b55f561154e533ad4e90c43c47e1a
SHTAKET1032 <SHTAKET1032@yandex.ru>
2023-12-19 17:18:04 +0300

    feat: added dynamic addition of cards

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ import Drawer from "./components/Drawer/Drawer";
 
 
 
+const data = [
+    {name: 'Nike Blazer Mid Suede', price: 12999, imgUrl: '/img-sneakers/1.jpg'},
+    {name: 'Nike Air Max 270', price: 12000, imgUrl: '/img-sneakers/2.jpg'},
+    {name: 'Under Armour Curry 8', price: 10000, imgUrl: '/img-sneakers/3.jpg'},
+    {name: 'Nike Kyrie 7', price: 15000, imgUrl: '/img-sneakers/4.jpg'},
+    {name: 'Jordan Air Jordan 11', price: 18000, imgUrl: '/img-sneakers/5.jpg'},
+    {name: 'Nike Lebron XVIII Low', price: 14500, imgUrl: '/img-sneakers/6.jpg'},
+    {name: 'Puma X Aka Boku Future Rider', price: 8900, imgUrl: '/img-sneakers/7.jpg'},
+    {name: 'Nike Kyrie Flytrap IV', price: 11900, imgUrl: '/img-sneakers/8.jpg'},
+]
+
+
 
 function App() {
     return (
@@ -28,12 +40,15 @@ function App() {
 
                 <div className="sneakers-list">
 
-                    <SneakerCard/>
-                    <SneakerCard/>
-                    <SneakerCard/>
-                    <SneakerCard/>
-                    <SneakerCard/>
+                    {data.map((item)=> (
 
+                        <SneakerCard
+                            name={item.name}
+                            price={item.price}
+                            imageUrl={item.imgUrl}
+                            priKlicke={()=>{console.log(`Вы кликнули на кроссовки ${item.name}`)}}
+                        />
+                    ))}
 
                 </div>
 
--- src/components/Sneaker-card/Sneaker-card.js
+++ src/components/Sneaker-card/Sneaker-card.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Sneaker-card.scss"
 
 
-function SneakerCard() {
+function SneakerCard(props) {
     return(
         <div className="sneaker-card">
 
@@ -10,18 +10,18 @@ function SneakerCard() {
                 <img src="/icons/heart-default.svg" alt="heart-unliked"/>
             </div>
 
-            <img src="/img-sneakers/1.jpg" alt="sneakers" className="sneaker-card-img"/>
-            <h5 className="sneaker-card-name">Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <img src={props.imageUrl} alt="sneakers" className="sneaker-card-img"/>
+            <h5 className="sneaker-card-name">{`Мужские Кроссовки ${props.name}`}</h5>
 
             <div className="sneaker-card-bottom">
 
                 <div className="price-container">
                     <span className="price-txt">Цена:</span>
-                    <b className="price-nmb">12 999 руб.</b>
+                    <b className="price-nmb">{props.price}.</b>
                 </div>
 
 
-                <img src="/icons/plus.svg" alt="plus-icon" className="btn-state-add btn-plus"/>
+                <img src="/icons/plus.svg" alt="plus-icon" className="btn-state-add btn-plus" onClick={props.priKlicke}/>
 
             </div>
 
